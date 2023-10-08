0b8250304dcd3db7ca758a2ad164dd19c299d4f3
bgillcode <b.gill.messages@gmail.com>
2023-10-08 21:25:56 +0100

    Enable or disable

---
+++ popup.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggleButton');
+
+  chrome.storage.local.get(['isActive'], (result) => {
+    toggleButton.innerText = result.isActive ? 'Disable' : 'Enable';
+  });
+
+  toggleButton.addEventListener('click', () => {
+    chrome.storage.local.get(['isActive'], (result) => {
+      const isActive = result.isActive === undefined ? true : !result.isActive;
+
+      chrome.storage.local.set({ isActive: isActive }, () => {
+        if (chrome.runtime.lastError) {
+          console.error(
+            'Error setting storage value:',
+            chrome.runtime.lastError,
+          );
+          return;
+        }
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          const currentTab = tabs[0];
+          const tabUrl = currentTab.url;
+
+          if (tabUrl.startsWith('http://') || tabUrl.startsWith('https://')) {
+            chrome.tabs.reload(currentTab.id);
+            window.close();
+          } else {
+            console.log('Page is not HTTP or HTTPS. Not reloading.');
+            window.close();
+          }
+        });
+      });
+    });
+  });
+});
